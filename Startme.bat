06817dddfbb9cab338bb8f5200e6fd3420ff7c95
BADII BAYOUDH <piterion.bayoudh@extaccount.com>
2024-07-25 16:55:16 +0200

    Batch - Version

---
+++ Startme.bat
@@ -0,0 +1,91 @@
+@echo off 
+REM *** Start multiple time the download client ***
+
+REM generate credentials-Datei
+
+
+REM Please change the paths below ..
+set "XML_INPUT_DIRECTORY=C:\ASPLM\FileDownloadClient\test\FDC-Konfigs\tmp_Badii_FDC_xml\"
+set "FILE_DOWNLOAD_CLIENT_HOME=C:\ASPLM\FileDownloadClient\test\FDC-Konfigs\"
+set "Log_OUTPUT=C:\ASPLM\FileDownloadClient\test\FDC-Konfigs\logs\"
+REM set "maxProc=28"
+set "maxProc=5"
+set "waitTimeBeforeClose=30"
+REM Thanks ..
+
+set "Launcher_HOME=%~dp0"
+echo %Launcher_HOME%
+echo "XML input directory: %XML_INPUT_DIRECTORY%"
+echo "Log output directory: %Log_OUTPUT%"
+for %%a in (%XML_INPUT_DIRECTORY%*) do call :loop %%a
+
+pause
+goto :eof
+
+
+:loop
+
+call :checkinstances
+
+if %INSTANCES% LSS %maxProc% goto :work
+
+rem wait a second, can be adjusted with -w (-n 2 because the first ping returns immediately;
+rem otherwise just use an address that's unused and -n 1)
+echo Waiting for instances to close ...
+ping -n 2 ::1 >nul 2>&1
+rem jump back to see whether we can spawn a new process now
+rem goto loop
+goto :loop
+
+:work	
+	set "configFilePath=%1"
+	set "configName=%~dpn1"
+
+	echo.
+	echo "-----------------------"
+	echo "| Run download client |
+	echo "-----------------------"
+	
+	echo "configFilePath: %configFilePath%"
+	echo "configName: %configName%"
+
+	
+	echo "config file path: %configFilePath%
+	for %%b in ("%configFilePath%") do set "configFileName=%%~nxa"
+	echo "Config file name: %configFileName%"
+	
+	for %%b in ("%configName%") do set "configName=%%~na"
+	echo "Config name: %configName%"
+
+	Set "Up2Sub=%configFileName:*_=%"
+	Set "baureihe=%Up2Sub:_="&:"%"
+	echo "Baureihe: %baureihe%"
+
+	set "LOG_FILE_PATH=%Log_OUTPUT%%Baureihe%\%configName%"
+	echo "log file path: %LOG_FILE_PATH%"
+
+	set LOGGING_FILE_NAME=%configName%
+	echo "logging file name: %configName%"
+
+	set "FDC_RUN_STATUS_PATH=%LOG_FILE_PATH%"
+
+	if not exist "%LOG_FILE_PATH%" mkdir %LOG_FILE_PATH%
+
+	start "download_client_instance" call "%Launcher_HOME%generic.bat"
+	
+    goto :eof
+
+:waitqueue
+rem wait a second, can be adjusted with -w (-n 2 because the first ping returns immediately;
+rem otherwise just use an address that's unused and -n 1)
+rem echo Waiting for instances to close ...
+ping -n 2 ::1 >nul 2>&1
+rem jump back to see whether we can spawn a new process now
+rem goto loop
+goto :loop
+	
+:checkinstances
+rem this could probably be done better. But INSTANCES should contain the number of running instances afterwards.
+for /f "usebackq" %%t in (`tasklist /fo csv /fi "imagename eq cmd.exe"^|find /v /c ""`) do set INSTANCES=%%t
+echo "current Number of running cmd.exe: %INSTANCES%"
+goto :eof
---
+++ generic.bat
@@ -0,0 +1,75 @@
+@ECHO OFF
+
+rem Modes supported in FDC :  encrypt_mode   and  download_mode
+
+rem  "FILE_DOWNLOAD_CLIENT_HOME=E:\FileDownloadclient\Downloadclient\"
+rem set "FILE_DOWNLOAD_CLIENT_HOME=%1"
+rem  "XML_INPUT_DIRECTORY=E:\FileDownloadclient\Konfigurationsdateien\"
+rem set "XML_INPUT_DIRECTORY=%2"
+
+rem replace <CHANGEME_USERPID> with pid
+rem replace <CHANGEME_ENVIRONMENT_TO_CONNECT> with TI, TI2 or PROD
+rem replace <CHANGEME_INPUT_FILE_NAME> with INPUT_XML
+
+rem Technical User PID which is used to launch FDC 
+set USERPID=pid5457  
+
+rem possible values for ENVIRONMENT_TO_CONNECT  : TI, TI2
+set ENVIRONMENT_TO_CONNECT=PROD
+
+rem PATH D:\FileDownloadclient\FileDownloadClient_Armin\jdk-11.0.2\bin;!PATH!
+rem set PATH=%PATH%;C:\Windows\System32\WindowsPowerShell\v1.0
+
+rem set JAVA_HOME=D:\FileDownloadclient\FileDownloadClient_Armin\jdk-11.0.2
+
+
+rem  LOG_FILE_PATH=E:\FileDownloadclient\Output\logs\C117\AS_C117_FC_L
+rem set LOG_FILE_PATH=%3
+
+rem enable to change application logfile name.
+rem  LOGGING_FILE_NAME=AS_C117_FC_L
+rem set LOGGING_FILE_NAME=%4
+
+rem enable to change path to the folder where excution status files should be generated
+rem default location:Location of jar file
+rem  FDC_RUN_STATUS_PATH=E:\FileDownloadclient\Output\logs\C117\AS_C117_FC_L
+rem set FDC_RUN_STATUS_PATH=%5
+ 
+set STATUS_CHECK_INTERVAL=5
+set MODE=%1   
+
+IF [%1] == [] GOTO SetDefaultParam
+IF %MODE% == encrypt_mode GOTO GetCredentials
+
+IF NOT [%1] == [] GOTO Execute
+
+:SetDefaultParam
+ECHO No param recieved. Setting mode to download_mode
+set MODE=download_mode
+GOTO Execute
+
+:GetCredentials
+for /F "usebackq" %%p in (`powershell -Command "$clientId = read-host 'Enter Client Id' -AsSecureString ;$BSTR=[System.Runtime.InteropServices.Marshal]::SecureStringToBSTR($clientId);[System.Runtime.InteropServices.Marshal]::PtrToStringAuto($BSTR)"`) do set clientId=%%p
+for /F "usebackq" %%p in (`powershell -Command "$clientSecret = read-host 'Enter Client Secret' -AsSecureString ;$BSTR=[System.Runtime.InteropServices.Marshal]::SecureStringToBSTR($clientSecret);[System.Runtime.InteropServices.Marshal]::PtrToStringAuto($BSTR)"`) do set clientSecret=%%p
+for /F "usebackq" %%p in (`powershell -Command "$xApiKey = read-host 'Enter X-API-Key' -AsSecureString ;$BSTR=[System.Runtime.InteropServices.Marshal]::SecureStringToBSTR($xApiKey);[System.Runtime.InteropServices.Marshal]::PtrToStringAuto($BSTR)"`) do set xApiKey=%%p
+GOTO Execute
+
+
+:Execute
+echo "Mode: %MODE%"
+set DOWNLOAD_ARGS=--encryptedCredLocation="%USERPROFILE%\Credentials\EncryptedCred_PROD.txt" --inputFileLocation="%XML_INPUT_DIRECTORY%%configFileName%"
+
+echo "Download args: %DOWNLOAD_ARGS%"
+
+C:"\apps\java\java17\bin\java" -Dfile.encoding=UTF-8 -jar "%FILE_DOWNLOAD_CLIENT_HOME%fdc_v6_26_06_2024.jar" %MODE% %DOWNLOAD_ARGS%
+
+rem C:"\Armin\03_Freeware\graalvm-ce-java17-22.3.0\bin\java" -Dfile.encoding=UTF-8 -jar "%FILE_DOWNLOAD_CLIENT_HOME%svc40-batch-app.jar" %MODE% %DOWNLOAD_ARGS%
+rem C:"\Program Files\EC\jre\bin\java" -Dfile.encoding=UTF-8 -jar "%FILE_DOWNLOAD_CLIENT_HOME%fdc_v6_26_06_2024.jar" %MODE% %DOWNLOAD_ARGS%
+
+rem pause
+echo Completed. The window will be closed in 30 Seconds
+echo.
+timeout /t %waitTimeBeforeClose%
+
+REM exit
+
